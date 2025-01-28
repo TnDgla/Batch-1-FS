b26a08c4911c5b2960112297d3f38abd1e5ca2a8
hemant12797 <hemantthakur8439@gmail.com>
2025-01-28 09:49:34 +0530

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,35 @@
+// conditional in javascript
+
+
+
+// IF ELSE CONDITIONS 
+let a=0;
+if(a%2==0 && a!=0){
+    console.log('even');
+}
+else if(a==0){
+    console.log('zero');
+}
+else{
+    console.log('odd');
+}
+
+
+
+// SWITCH CONDITION 
+a=3;
+switch(a){
+    case 1:
+        console.log('1');
+        return;
+    case 2:
+        console.log('2');
+        return;
+    case 3:
+        console.log('this 3');
+        return;
+    case 4:
+        console.log('4');
+        return;
+
+}
